b7281c36c8a0d37bd1dc3f7be39f3ac90f98a37b
Erin Kelley <erinmkelley7@gmail.com>
2017-08-10 13:48:41 -0400

    refactor

---
+++ app/src/main/java/com/me/sfweather/utilities/WeatherConst.java
@@ -0,0 +1,67 @@
+package com.me.sfweather.utilities;
+
+/**
+ * Created by erin.kelley on 8/10/17.
+ */
+
+public class WeatherConst {
+    // Intent Strings
+    public static final String HOURLY_DATA_RECEIVED_FILTER = "Hourly Data Received";
+    public static final String CURRENT_DATA_RECEIVED_FILTER = "Current Data Received";
+    public static final String EXTENDED_DATA_RECEIVED_FILTER = "Extended Data Received";
+    public static final String HOURLY_JSON_DATA = "Hourly JSON Data";
+    public static final String CURRENT_JSON_DATA = "Current JSON Data";
+    public static final String EXTENDED_JSON_DATA = "Extended JSON Data";
+    public static final String ACTION_UPDATE_HOURLY_VIEWS = "action update hourly views";
+    public static final String ACTION_UPDATE_CURRENT_VIEWS = "action update current views";
+    public static final String ACTION_UPDATE_EXTENDED_VIEWS = "action update extended views";
+    public static final String HOURLY_DATA = "Hourly Data";
+    public static final String CURRENT_DATA = "Current Data";
+    public static final String EXTENDED_DATA = "Extended Data";
+
+    // Hourly Strings
+    public static final String TIMES = "times";
+    public static final String TEMPS = "temps";
+    public static final String PRECIPS = "precips";
+    public static final String WIND_SPEEDS = "wind speeds";
+    public static final String WIND_DIRS = "wind directions";
+    public static final String CONDITIONS = "conditions";
+
+    // Current Strings
+    public static final String FEELS_LIKE = "feels like";
+    public static final String CONDITION = "condition";
+    public static final String CONDITION_DESC = "condition description";
+    public static final String WIND = "wind";
+    public static final String HUMIDITY = "humidity";
+    public static final String DEW_POINT = "dew point";
+    public static final String PRESSURE = "pressure";
+    public static final String UV_INDEX = "uv index";
+
+    // Extended Strings
+    public static final String DAYS_OF_WEEK = "days of week";
+    public static final String DESCS = "descriptions";
+    public static final String DATES = "dates";
+    public static final String HIGH_TEMPS = "high temps";
+    public static final String LOW_TEMPS = "low temps";
+    public static final String DAY_WIND_MPHS = "day wind mphs";
+    public static final String NIGHT_WIND_MPHS = "night wind mphs";
+    public static final String DAY_WIND_DIRS = "day wind dirs";
+    public static final String NIGHT_WIND_DIRS = "night wind dirs";
+    public static final String COND_DESCS = "condition descriptions";
+
+    // Conditions
+    public static final String CLEAR_DAY = "clear";
+    public static final String PARTLY_CLOUDY_DAY = "partlycloudy";
+    public static final String MOSTLY_CLOUDY_DAY = "mostlycloudy";
+    public static final String CHANCE_T_STORMS_DAY = "chancetstorms";
+    public static final String CHANCE_RAIN_DAY = "chancerain";
+    public static final String CLEAR_NIGHT = "nt_clear";
+    public static final String PARTLY_CLOUDY_NIGHT = "nt_partlycloudy";
+    public static final String MOSTLY_CLOUDY_NIGHT= "nt_mostlycloudy";
+    public static final String CHANCE_T_STORMS_NIGHT = "nt_chancetstorms";
+    public static final String CHANCE_RAIN_NIGHT = "nt_chancerain";
+    
+    // Symbols
+    public static final String DEGREE_SYMBOL = " \u2109";
+    public static final String PERCENT_SYMBOL = " %";
+}
